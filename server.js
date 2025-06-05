856d1c758e8bb99ddc57f8136ae13485598af92a
NokiaZ <49866374+zyh10655@users.noreply.github.com>
2025-06-05 14:06:52 +1200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs').promises;
 const markdownPdf = require('markdown-pdf');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // CORS configuration
 app.use(cors({
